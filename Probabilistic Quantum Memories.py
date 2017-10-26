2568352cb2de6f38ef027287d8505b9bd87f82a4
Leonardo <leonardo44ls@gmail.com>
2017-10-26 20:06:29 -0300

    Primeiro commit com o código

---
+++ Probabilistic Quantum Memories.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 10:28:46 2017
+
+@author: leonardo
+"""
+
+from sympy.physics.quantum.qubit import Qubit
+from sympy.physics.quantum.tensorproduct import TensorProduct
+import numpy
+
+def twoXor(qubit):
+    
+    qubitTemp = Qubit(qubit)
+    
+    for i in range(numeroBits):
+        if( qubitTemp[indiceP + i] == 1 and qubitTemp[indiceU2] == 1 ):
+            qubitTemp = qubitTemp.flip(indiceM + i)
+    
+    return qubitTemp
+
+def notXor(qubit):
+    
+    qubitTemp = Qubit(qubit)
+    
+    for i in range(numeroBits):
+        if( qubitTemp[indiceP + i] == 1 ):
+            qubitTemp = qubitTemp.flip(indiceM + i)
+        
+        qubitTemp = qubitTemp.flip(indiceM + i)
+    
+    return qubitTemp
+
+################# Execucao #############################
+
+entrada = ['101', '001', '000']
+    
+numeroBits = len(entrada[0])
+indiceU1 = numeroBits + 1
+indiceU2 = numeroBits
+
+indiceP = indiceU1 + 1
+indiceM = 0
+
+memoria = ''
+
+for i in range(numeroBits):
+    memoria += '0'
+
+qubitInicial = Qubit(entrada[0] + '01' + memoria)
+
+print (qubitInicial)
+
+saida1 = twoXor(qubitInicial)
+
+print (saida1)
+
+saida2 = notXor(saida1)
+
+print (saida2)
